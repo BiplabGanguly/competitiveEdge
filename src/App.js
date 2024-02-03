6d85c73db3d0a9ad859a5324fc3a1af577f18aca
biplab ganguly <biplab123.abcd@gamil.com>
2024-02-03 21:18:14 +0530

    add component folder and test component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Test from './components/test';
 
 function App() {
   return (
     <div className="App">
       <h1>Abhijit</h1>
+      <Test/>
     </div>
   );
 }
