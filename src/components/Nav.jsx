c970e976e153f5887ec8a949a5fc032b978b157d
biplab ganguly <biplab123.abcd@gamil.com>
2024-02-10 00:17:45 +0530

    add bootstrap

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,6 +8,7 @@ function Nav() {
       <Link to="/institute">
         <h2>institution</h2>
       </Link>
+      <div className="container" style={{ border: "1px solid black" }}>test</div>
     </div>
   );
 }
