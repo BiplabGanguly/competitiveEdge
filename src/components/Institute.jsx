375215a0808363a9455624202698fdc5048ea582
biplab ganguly <biplab123.abcd@gamil.com>
2024-02-10 02:02:59 +0530

    add institute css and navbar

--- src/components/Institute.jsx
+++ src/components/Institute.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "../CSSfolder/instituteStyle.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "../CSSfolder/instituteStyle.css";
+import Nav from "./Nav";
 
 function Institute() {
   return (
-    <div>
-      <div classname="container">
-        <div className='row'>
-          <div className='col-md-6'>
-            <div className='adminsection'>
-              <Link to="/admin-login">admin</Link>
+    <>
+    <Nav/>
+      <div className="container maincon">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="adminsection">
+              <Link to="/admin-login">Admin</Link>
             </div>
           </div>
-          <div className='col-md-6'>Faculty</div>
+          <div className="col-md-6">
+            <div className="adminsection">Faculty</div>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    </>
+  );
 }
 
-export default Institute
+export default Institute;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,15 +1,56 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../CSSfolder/navStyle.css"
 
 function Nav() {
   return (
-    <div>
-      {" "}
-      <Link to="/institute">
-        <h2>institution</h2>
-      </Link>
-      <div className="container" style={{ border: "1px solid black" }}>test</div>
-    </div>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <div className="navbar-brand" href="#">
+          Navbar
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <li className="nav-item">
+              <Link to="/" className="institute"><div className="nav-link">Home</div></Link>
+            </li>
+            <li className="nav-item">
+              <div className="nav-link" href="#">
+                About
+              </div>
+            </li>
+            <li className="nav-item">
+              <div className="nav-link">Our team</div>
+            </li>
+            <li className="nav-item">
+              <Link to="/institute" className="institute"><div className="nav-link">Institute</div></Link>
+            </li>
+          </ul>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </div>
+    </nav>
   );
 }
 
