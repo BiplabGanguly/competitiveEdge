b0082da79914671802c5614a29e47fb42b39c53d
Bapan1906 <bapandutta19062001@gmail.com>
2024-02-15 20:43:25 +0530

    Changes

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 import "../CSSfolder/aboutStyle.css";
-import Nav from "../components/Nav";
 
-
-
-function About(){
-    return(
-        <>
-        <Nav/>
-        <div className="container">
+function About() {
+  return (
+    <>
+      <div className="container">
         <div className="row">
-          <div className="col-md-12">
-          Lorendkjdhkuhdskhdsihdsh
-          </div>
-          </div>
-          </div>
-
-        </>
-    )
+          <div className="col-md-12">Lorendkjdhkuhdskhdsihdsh</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default About;
