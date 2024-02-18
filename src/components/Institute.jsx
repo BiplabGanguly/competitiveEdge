05af7c7be8ad25aafe17116a98a1a49025d36298
Abhijit-1234 <abhijitdutta31102001@gmail.com>
2024-02-18 23:57:20 +0530

    about page modification

---
+++ src/components/Institute.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../CSSfolder/instituteStyle.css";
+
+function Institute() {
+  return (
+    <>
+      <div className="container maincon">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="adminsection">
+              <Link to="/admin-login">Admin</Link>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="adminsection">Faculty</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Institute;
--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -1,17 +1,42 @@
 import React from "react";
 import "../CSSfolder/aboutStyle.css";
-import Nav from "../components/Nav";
+import aboutimg from "../Public/about.png"
 
 
 
 function About(){
     return(
         <>
-        <Nav/>
         <div className="container">
-        <div className="row">
+          <div className="row">
+          <div className="col-md-6">
+            <div className="about-col-img">
+            <img src={aboutimg} alt="about" className="img-fluid" />
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="about-col-content">
+            <div className="about-title">About</div>
+              <div className="about-para">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Quae aut at provident aliquam fugit. Debitis quasi obcaecati
+                deserunt repudiandae nulla asperiores optio nam voluptatem hic,
+                ex vero doloribus earum facere.
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dignissimos quisquam optio error praesentium accusantium aliquid
+                minus quae similique tempora dicta dolorum et, laborum corrupti ea eum officia esse.
+                Assumenda, praesentium?
+            </div>
+          </div>
+          </div>
+          <div className="row">
           <div className="col-md-12">
-          Lorendkjdhkuhdskhdsihdsh
+          <div className="about-lower-content">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Dignissimos quisquam optio error praesentium accusantium aliquid
+                minus quae similique tempora dicta dolorum et, laborum corrupti ea eum officia esse.
+                Assumenda, praesentium?
+            </div>
+            </div>
           </div>
           </div>
           </div>
