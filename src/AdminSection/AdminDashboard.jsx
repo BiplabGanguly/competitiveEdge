57c1fef0f71eb587de8dd113ab32e42ac65dba8a
Abhijit-1234 <abhijitdutta31102001@gmail.com>
2024-05-13 01:38:03 +0530

    admin dashboard create table

--- src/AdminSection/AdminDashboard.jsx
+++ src/AdminSection/AdminDashboard.jsx
@@ -16,19 +16,66 @@ function AdminDashboard() {
             </div>
             <div className='row center-content'>
                 <div className='col-md-4 branch-div'>
-                <AdminDashboardInfobox heading="Branches" infonumber="07" infodata="There are 07 Branches present"/>
+                    <AdminDashboardInfobox heading="Branches" infonumber="07" infodata="There are 07 Branches present" />
                 </div>
                 <div className='col-md-4 faculty-div'>
-                <AdminDashboardInfobox heading="Faculty" infonumber="07" infodata="There are 07 Branches present"/>
+                    <AdminDashboardInfobox heading="Faculty" infonumber="07" infodata="There are 07 Branches present" />
 
                 </div>
                 <div className='col-md-4 student-div'>
-                <AdminDashboardInfobox heading="Student" infonumber="07" infodata="There are 07 Branches present"/>
+                    <AdminDashboardInfobox heading="Student" infonumber="07" infodata="There are 07 Branches present" />
                 </div>
             </div>
-            <div className='row'>
-                <div className='col-md-12 other-content'>
-                    kjhafliejoljefkjdsf
+            <div className='row mt-5'>
+                <div className='table-responsive'>
+                    <table className='table table-bordered table-striped table-hover table-sm'>
+                        <thead className='thead-dark'>
+                            <tr>
+                                <th>Faculty Id</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Branch</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>01</td>
+                                <td>Abhi</td>
+                                <td>abhi783@gmail.com</td>
+                                <td>MCA</td>
+                                <td>Active</td>
+                            </tr>
+                            <tr>
+                                <td>02</td>
+                                <td>bapan</td>
+                                <td>bapan3783@gmail.com</td>
+                                <td>MBA</td>
+                                <td>Active</td>
+                            </tr>
+                            <tr>
+                                <td>03</td>
+                                <td>Biplab</td>
+                                <td>bip3783@gmail.com</td>
+                                <td>B-tech</td>
+                                <td>Active</td>
+                            </tr>
+                            <tr>
+                                <td>04</td>
+                                <td>Tamal</td>
+                                <td>tamsld3783@gmail.com</td>
+                                <td>BBA</td>
+                                <td>Active</td>
+                            </tr>
+                            <tr>
+                                <td>05</td>
+                                <td>Ram</td>
+                                <td>ram73847@gmail.com</td>
+                                <td>M-tech</td>
+                                <td>Active</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
