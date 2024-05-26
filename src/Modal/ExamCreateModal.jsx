3522ac007f5826e2cf6aec7a294b452a0da28d34
biplabganguly <biplab123.abcd@gmail.com>
2024-05-27 00:14:23 +0530

    modify exam create modal

--- src/Modal/ExamCreateModal.jsx
+++ src/Modal/ExamCreateModal.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../Css/examCreateModalStyle.css'
 
 function ExamCreateModal() {
     return (
@@ -9,13 +10,50 @@ function ExamCreateModal() {
                         <h5 className="modal-title" id="examCreateModal">Create Mock Exam</h5>
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
-                    <div className="modal-body">
-                        
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" className="btn btn-primary">Save changes</button>
-                    </div>
+                    <form className='createExamModal'>
+                        <div className="modal-body">
+
+                            <div className="col-md-12 mb-3">
+                                <input type="text" className='form-control' placeholder='exam name' />
+                            </div>
+                            <div className="col-md-12 mb-3">
+                                <textarea className='form-control' placeholder='exam description'></textarea>
+                            </div>
+                            <input readOnly className='form-control mb-3' />
+                            <div className="col-md-12 mb-3">
+                                <input type="text" className='form-control' placeholder='passkey' />
+                            </div>
+                            <div className="row mb-3">
+                                <div className="col-md-3">
+                                    <span>Exam date</span>
+                                </div>
+                                <div className="col-md-9">
+                                    <input type="date" className="form-control" />
+                                </div>
+                            </div>
+                            <div className="row mb-3">
+                                <div className="col-md-3">
+                                    <span>Start time</span>
+                                </div>
+                                <div className="col-md-9">
+                                    <input type="time" className="form-control" />
+                                </div>
+                            </div>
+
+                            <div className="row mb-3">
+                                <div className="col-md-3">
+                                    <span>End time</span>
+                                </div>
+                                <div className="col-md-9">
+                                    <input type="time" className="form-control" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">create exam</button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
