b71019fdce7d0f84d84e50fd4ed11c0d62f8346d
Bapan1906 <bapandutta19062001@gmail.com>
2024-05-31 21:29:20 +0530

    Student Registration complete

--- src/components/StudentLogin.jsx
+++ src/components/StudentLogin.jsx
@@ -3,18 +3,19 @@ import { Link } from "react-router-dom";
 import "../Css/StudentLogin.css";
 import studentloginimage from "../Public/Student_Login_Image.png";
 
-function AdminLoginForm(event) {
-  event.preventDefault();
-  // Add your form submission logic here
-}
-
 function StudentLogin() {
+  const handleLoginFormSubmit = (event) => {
+    event.preventDefault();
+    // Add your form submission logic here
+    console.log("Form submitted");
+  };
+
   return (
     <div className="container Student-Login-Container">
       <div className="row login_row">
         <div className="col-lg-6 login_form">
           <div className="row">
-            <form onSubmit={AdminLoginForm}>
+            <form onSubmit={handleLoginFormSubmit}>
               <div className="col-md-12 stdLogin_Title">Student login</div>
               <hr />
               <div className="col-md-12">
@@ -53,12 +54,11 @@ function StudentLogin() {
             </form>
           </div>
         </div>
-
         <div className="col-lg-6">
           <div className="login-col-img">
             <img
               src={studentloginimage}
-              alt="adminlogin"
+              alt="student login"
               className="img-adminlogin"
             />
           </div>
