ec6aa6b7188817fa1b98a5e01b23c6eca0513af8
Bapan1906 <bapandutta19062001@gmail.com>
2024-06-03 00:35:33 +0530

    Final commit

--- src/components/StudentLogin.jsx
+++ src/components/StudentLogin.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "../Css/StudentLogin.css";
 import studentloginimage from "../Public/Student_Login_Image.png";
 
@@ -9,62 +9,85 @@ function AdminLoginForm(event) {
 }
 
 function StudentLogin() {
-  return (
-    <div className="container Student-Login-Container">
-      <div className="row login_row">
-        <div className="col-lg-6 login_form">
-          <div className="row">
-            <form onSubmit={AdminLoginForm}>
-              <div className="col-md-12 stdLogin_Title">Student login</div>
-              <hr />
-              <div className="col-md-12">
-                <input
-                  type="email"
-                  className="form-control input_box"
-                  placeholder="email"
-                  required
-                />
-              </div>
-              <div className="col-md-12">
-                <input
-                  type="text"
-                  className="form-control input_box"
-                  placeholder="username"
-                  required
-                />
-              </div>
-              <div className="col-md-12">
-                <input
-                  type="password"
-                  className="form-control input_box"
-                  placeholder="Password"
-                  required
-                />
-              </div>
-              <div className="col-md-12">
-                <button type="submit" className="btn btn-home">
-                  Signin
-                </button>
-              </div>
-              <div className="col-md-12 signup-text-login">
-                Don't have an account yet?{" "}
-                <Link to="/studentRegistration">Sign up now</Link>
-              </div>
-            </form>
-          </div>
-        </div>
+  const [email, setEmail] = useState("example@gmail.com");
+  const [username, setUsername] = useState("eample123");
+  const [password, setPassword] = useState("Example@11");
 
-        <div className="col-lg-6">
-          <div className="login-col-img">
-            <img
-              src={studentloginimage}
-              alt="adminlogin"
-              className="img-adminlogin"
-            />
-          </div>
+  const navigate = useNavigate();
+
+  const StudentLoginForm = async (event) => {
+    event.preventDefault();
+
+
+  }
+
+  return (
+    <div className="container student-form-login-container">
+    <div className="row">
+      <div className="login-title">Login</div>
+    </div>
+
+    <div className="row login_row">
+      <div className="col-lg-6 login_form">
+        <div className="row">
+          <form onSubmit={StudentLoginForm}>
+            <div className="col-md-12 login-form-title">Student Login</div>
+            <hr></hr>
+            <div className="col-md-12">
+              <input
+                type="email"
+                className="form-control input-box"
+                placeholder="example@gmail.com,"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="col-md-12">
+              <input
+                type="text"
+                className="form-control input-box"
+                placeholder="eample123,"
+                required
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="col-md-12">
+              <input
+                type="password"
+                className="form-control input-box"
+                placeholder="Example@11"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="col-md-12">
+              <button type="submit" className="btn btn-home">
+                Signin
+              </button>
+            </div>
+            <div className="col-md-12 signup-text-login">
+              Don't have an account yet?{" "}
+              <Link to="/studentRegistration"> Sign up now </Link>
+            </div>
+          </form>
         </div>
       </div>
+
+      <div className="col-lg-6">
+        <div className="login-col-img">
+          <img
+            src={studentloginimage}
+            alt="adminlogin"
+            className="img-adminlogin"
+          />
+        </div>
+      </div>
+      
     </div>
+  </div>
   );
 }
 
